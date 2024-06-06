f95db499801631d131a3b447ac72c0c9f85b07eb
Hanze Liu <liuhanzecrystal@yahoo.com>
2024-06-06 17:32:13 -0400

    minor change

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(function (req, res, next) {
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello, World!!");
 });
 
 app.get("/api/projects", async (req, res) => {
